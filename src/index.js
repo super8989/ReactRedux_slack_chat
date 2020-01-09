ca26fb1aabdeeb6a90ac3824f8ebfe1df582d6c2
Sam <super8989@gmail.com>
2020-01-09 17:35:32 -0500

    creating a register form in Register.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 
 const Root = () => (
 	<Router>
