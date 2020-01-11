15f1d44924f7ef9ffe5b27eb9c93ebaec4b86f81
Sam <super8989@gmail.com>
2020-01-11 15:54:00 -0500

    created folders and components scaffolded in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
 import "./App.css";
 
 const App = () => (
