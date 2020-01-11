f406c3cc64cdda7b4a4ce65e78e2299b8c4b404b
Sam <super8989@gmail.com>
2020-01-10 19:18:00 -0500

    imported firebase to index.js and changed it into class component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import firebase from "./firebase";
 
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
 import "semantic-ui-css/semantic.min.css";
 
-const Root = () => (
-	<Router>
-		<Switch>
-			<Route exact path='/' component={App} />
-			<Route path='/login' component={Login} />
-			<Route path='/register' component={Register} />
-		</Switch>
-	</Router>
-);
+class Root extends Component {
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path='/' component={App} />
+					<Route path='/login' component={Login} />
+					<Route path='/register' component={Register} />
+				</Switch>
+			</Router>
+		);
+	}
+}
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 
