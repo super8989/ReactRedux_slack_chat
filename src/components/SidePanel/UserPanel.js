a32171062cb6987458568fcc0004046d7b846506
Sam <super8989@gmail.com>
2020-01-11 16:23:49 -0500

    added a dropdown to UserPanel.js

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
-import { Grid, Header, Icon } from "semantic-ui-react";
+import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
 
 class UserPanel extends Component {
+	dropdownOptions = () => [
+		{
+			key: "user",
+			text: (
+				<span>
+					Signed in as <strong>User</strong>
+				</span>
+			),
+			disabled: true
+		},
+		{
+			key: "avatar",
+			text: <span>Change Avatar</span>
+		},
+		{
+			key: "signout",
+			text: <span>Sign Out</span>
+		}
+	];
+
 	render() {
 		return (
 			<Grid style={{ background: "#4c3c4c" }}>
@@ -13,6 +33,14 @@ class UserPanel extends Component {
 							<Header.Content>DevChat</Header.Content>
 						</Header>
 					</Grid.Row>
+
+					{/* User Dropdown */}
+					<Header style={{ padding: "0.25em" }} as='h4' inverted>
+						<Dropdown
+							trigger={<span>User</span>}
+							options={this.dropdownOptions()}
+						/>
+					</Header>
 				</Grid.Column>
 			</Grid>
 		);
