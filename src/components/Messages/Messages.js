e64296f7d8bc10a5ab5cc62fb75d286c5065910b
Sam <super8989@gmail.com>
2020-01-11 23:10:59 -0500

    added MessageForm component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Segment, Comment } from "semantic-ui-react";
 
 import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
 
 class Messages extends Component {
 	render() {
@@ -12,7 +13,7 @@ class Messages extends Component {
 					<Comment.Group className='messages'>{/* Messages */}</Comment.Group>
 				</Segment>
 
-				{/* <MessageForm /> */}
+				<MessageForm />
 			</>
 		);
 	}
