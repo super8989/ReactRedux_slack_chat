8ee76333f711549d73a622229fe3b049f22c11d3
Sam <super8989@gmail.com>
2020-01-12 12:40:12 -0500

    imported mimetype validation for files and putting media files in state

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -63,6 +63,10 @@ class MessageForm extends Component {
 		}
 	};
 
+	uploadFile = (file, metadata) => {
+		console.log(file, metadata);
+	};
+
 	render() {
 		const { errors, message, loading, modal } = this.state;
 
@@ -99,7 +103,11 @@ class MessageForm extends Component {
 						labelPosition='right'
 						icon='cloud upload'
 					/>
-					<FileModal modal={modal} closeModal={this.closeModal} />
+					<FileModal
+						modal={modal}
+						closeModal={this.closeModal}
+						uploadFile={this.uploadFile}
+					/>
 				</Button.Group>
 			</Segment>
 		);
