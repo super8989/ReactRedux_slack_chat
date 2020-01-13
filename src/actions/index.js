8094a7d4bcad05611d1417320bae7912c7194658
Sam <super8989@gmail.com>
2020-01-12 20:49:53 -0500

    added DirectMessage channel data to global state and set private channels

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import * as actionTypes from "./types";
+import * as actionTypes from './types';
 
 // User Actions
 export const setUser = user => {
@@ -21,5 +21,14 @@ export const setCurrentChannel = channel => {
 	return {
 		type: actionTypes.SET_CURRENT_CHANNEL,
 		payload: { currentChannel: channel }
+	};
+};
+
+export const setPrivateChannel = isPrivateChannel => {
+	return {
+		type: actionTypes.SET_PRIVATE_CHANNEL,
+		payload: {
+			isPrivateChannel
+		}
 	};
 };
--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setCurrentChannel, setPrivateChannel } from '../../actions';
 
 class DirectMessages extends Component {
 	state = {
@@ -68,6 +70,23 @@ class DirectMessages extends Component {
 
 	isUserOnline = user => user.status === 'online';
 
+	changeChannel = user => {
+		const channelId = this.getChannelId(user.uid);
+		const channelData = {
+			id: channelId,
+			name: user.name
+		};
+		this.props.setCurrentChannel(channelData);
+		this.props.setPrivateChannel(true);
+	};
+
+	getChannelId = userId => {
+		const currentUserId = this.state.user.uid;
+		return userId < currentUserId
+			? `${userId}/${currentUserId}`
+			: `${currentUserId}/${userId}`;
+	};
+
 	render() {
 		const { users } = this.state;
 
@@ -84,7 +103,7 @@ class DirectMessages extends Component {
 				{users.map(user => (
 					<Menu.Item
 						key={user.uid}
-						onClick={() => console.log(user)}
+						onClick={() => this.changeChannel(user)}
 						style={{ opacity: 0.7, fontStyle: 'italic' }}
 					>
 						<Icon
@@ -99,4 +118,6 @@ class DirectMessages extends Component {
 	}
 }
 
-export default DirectMessages;
+export default connect(null, { setCurrentChannel, setPrivateChannel })(
+	DirectMessages
+);
