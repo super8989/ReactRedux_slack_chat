27f825c3c204a9a2d86096834132ff16271b640c
Sam <super8989@gmail.com>
2020-01-13 20:58:07 -0500

    changed the icon back to star

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -17,7 +17,7 @@ class MessagesHeader extends Component {
 				<Header fluid='true' as='h2' floated='left' style={{ maginBottom: 0 }}>
 					<span>
 						{channelName}{' '}
-						{!isPrivateChannel && <Icon name={'tv'} color='black' />}
+						{!isPrivateChannel && <Icon name={'star outline'} color='black' />}
 					</span>
 					<Header.Subheader>{numUniqueUsers}</Header.Subheader>
 				</Header>
