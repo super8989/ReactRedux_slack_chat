38526fdfa38048f8772d2cf92644326b19e5d2fb
Sam <super8989@gmail.com>
2020-01-14 21:36:03 -0500

    scaffolding the color picker modal in ColorPanel

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,26 @@
-import React, { Component } from "react";
-import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
+import React, { Component } from 'react';
+import {
+	Sidebar,
+	Menu,
+	Divider,
+	Button,
+	Modal,
+	Icon,
+	Label
+} from 'semantic-ui-react';
 
 class ColorPanel extends Component {
+	state = {
+		modal: true
+	};
+
+	openModal = () => this.setState({ modal: true });
+
+	closeModal = () => this.setState({ modal: false });
+
 	render() {
+		const { modal } = this.state;
+
 		return (
 			<Sidebar
 				as={Menu}
@@ -13,7 +31,23 @@ class ColorPanel extends Component {
 				width='very thin'
 			>
 				<Divider />
-				<Button icon='add' size='small' color='blue' />
+				<Button icon='add' size='small' color='blue' onClick={this.openModal} />
+
+				{/* Color Picker Modal */}
+				<Modal basic open={modal} onClose={this.closeModal}>
+					<Modal.Header>Choose App Colors</Modal.Header>
+					<Modal.Content></Modal.Content>
+					<Modal.Actions>
+						<Button color='green' inverted>
+							<Icon name='checkmark' />
+							Save Colors
+						</Button>
+						<Button color='red' inverted>
+							<Icon name='remove' />
+							Cancel
+						</Button>
+					</Modal.Actions>
+				</Modal>
 			</Sidebar>
 		);
 	}
