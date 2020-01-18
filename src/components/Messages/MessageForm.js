b770da87f677c17ff41802bd3b719b705a61fc03
Sam <super8989@gmail.com>
2020-01-17 21:55:48 -0500

    after selecting emoji, focus goes back to input area and emoji-mart closes automatically

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -56,6 +56,7 @@ class MessageForm extends Component {
 		const oldMessage = this.state.message;
 		const newMessage = this.colonToUnicode(` ${oldMessage} ${emoji.colons} `);
 		this.setState({ message: newMessage, emojiPicker: false });
+		setTimeout(() => this.messageInputRef.focus(), 0);
 	};
 
 	colonToUnicode = message => {
@@ -216,8 +217,15 @@ class MessageForm extends Component {
 					onChange={this.handleChange}
 					onKeyDown={this.handleKeyDown}
 					value={message}
+					ref={node => (this.messageInputRef = node)}
 					style={{ marginBottom: '0.7em' }}
-					label={<Button icon={'add'} onClick={this.handleTogglePicker} />}
+					label={
+						<Button
+							icon={emojiPicker ? 'close' : 'add'}
+							content={emojiPicker ? 'Close' : null}
+							onClick={this.handleTogglePicker}
+						/>
+					}
 					labelPosition='left'
 					className={
 						errors.some(error => error.message.includes('message'))
